e5d6eccf2c603df8d9e1f092bf2df2875811b461
pikachu987 <pikachu77769@gmail.com>
2018-11-21 23:31:27 +0900

    search init commit

---
+++ 1. SearchExample/SearchExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchExample
+//
+//  Created by Gwanho Kim on 21/11/2018.
+//  Copyright © 2018 Gwanho Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
