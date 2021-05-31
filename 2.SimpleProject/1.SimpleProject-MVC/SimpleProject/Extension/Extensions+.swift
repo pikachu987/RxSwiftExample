bcff818355a1ed5b21445f98ce273fdb2f9407ef
pikachu987 <pikachu77769@gmail.com>
2021-05-31 17:41:18 +0900

    simple proj 삭제

--- 2.SimpleProject/1.SimpleProject-MVC/SimpleProject/Extension/Extensions+.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Extensions+.swift
-//  SimpleProject
-//
-//  Created by Gwanho Kim on 24/11/2018.
-//  Copyright © 2018 Gwanho Kim. All rights reserved.
-//
-
-import Foundation
--- 3.SomeProject/1.RefreshProject/SomeProject/Extension/Extensions+.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Extensions+.swift
-//  SomeProject
-//
-//  Created by Gwanho Kim on 25/11/2018.
-//  Copyright © 2018 Gwanho Kim. All rights reserved.
-//
-
-import Foundation
